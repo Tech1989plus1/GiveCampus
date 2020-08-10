58d1450054eee8c70a97787501c065501b717270
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-08-10 13:33:36 -0700

    created logic in javascript

--- src/client/donors.js
+++ src/client/donors.js
@@ -1,10 +1,19 @@
 const ourRequest = new XMLHttpRequest();
-
 ourRequest.open('GET', 'http://localhost:3001/donors');
+const donorMax = 25000;
 
 ourRequest.onload = () => {
-  var donorsData = JSON.parse(ourRequest.responseText);
-  
+  const donorsData = JSON.parse(ourRequest.responseText);
+
+  const totalDonors = donorsData.length;
+  var totalDonated = 0;
+  var precentageDonated = 0;
+
+  for (let i = 0; i < totalDonors; i++) {
+    totalDonated += donorsData[i].amount;
+  }
+
+  precentageDonated = Math.floor((totalDonated / donorMax) * 100);
 };
 
 ourRequest.send();
