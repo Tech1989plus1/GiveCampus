3ebe2c4e048e5d75f9acc8829a839e19f61b23b4
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-08-11 11:00:15 -0700

    adding top ten donors logic

--- src/client/donors.js
+++ src/client/donors.js
@@ -9,8 +9,17 @@ ourRequest.onload = () => {
   var totalDonated = 0;
   var precentageDonated = 0;
 
+  var tableData = '<tr><th>Donor</th> <th>Dollars</th> <th>Type</th></tr>';
+
+  donorsData.sort((a ,b) => b.amount - a.amount);
+
   for (let i = 0; i < totalDonors; i++) {
     totalDonated += donorsData[i].amount;
+  }
+
+  for (let i = 0; i < 10; i++) {
+    let donor = donorsData[i];
+    tableData += `<tr><td>${donor.name}</td> <td>${donor.amount}</td> <td>${donor.type}</td></tr>`;
   }
 
   precentageDonated = Math.floor((totalDonated / donorMax) * 100);
@@ -20,6 +29,7 @@ ourRequest.onload = () => {
   document.getElementById('barPrecentage').innerHTML = precentageDonated.toString() + '%';
   document.getElementById('barPrecentage').style.width = precentageDonated.toString() + '%';
   document.getElementById('donated').innerHTML = '$' + totalDonated.toString();
+  document.getElementById('topDonors').innerHTML = tableData;
 };
 
 ourRequest.send();
